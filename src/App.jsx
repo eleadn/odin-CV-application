c2d4191046a8956a3a01dea4846f9caefa27debb
eleadn <eleano.dn@gmail.com>
2025-08-19 21:36:00 +0200

    Add education form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import EducationForm from "./forms/EducationForm";
-import Form from "./forms/Form";
 import InformationsForm from "./forms/InformationsForm";
 
 function App() {
 	return (
 		<>
 			<InformationsForm />
+			<EducationForm />
 		</>
 	);
 }
--- src/forms/EducationForm.jsx
+++ src/forms/EducationForm.jsx
@@ -1,35 +1,142 @@
 import { useState } from "react";
 
-export default function EducationForm({ isEditing }) {
+export default function EducationForm() {
 	const [education, setEducation] = useState([]);
+	const [draft, setDraft] = useState([...education]);
+
+	const [isEditing, setIsEditing] = useState(false);
 
 	const addEducation = () => {
 		const id = crypto.randomUUID();
 		const name = "";
 		const title = "";
 		const date = null;
-		setEducation([...education, { id, name, title, date }]);
+
+		const newDraft = [...draft];
+		newDraft.push({ id, name, title, date });
+		setDraft(newDraft);
+	};
+
+	const removeEducation = (id) => {
+		const newDraft = [...draft].filter((school) => school.id !== id);
+		setDraft(newDraft);
+	};
+
+	const onSchoolNameChange = (event, index) => {
+		const newDraft = [...draft];
+		newDraft[index].name = event.target.value;
+		setDraft(newDraft);
+	};
+
+	const onSchoolTitleChange = (event, index) => {
+		const newDraft = [...draft];
+		newDraft[index].title = event.target.value;
+		setDraft(newDraft);
+	};
+
+	const onSchoolDateChange = (event, index) => {
+		const newDraft = [...draft];
+		newDraft[index].date = event.target.value;
+	};
+
+	const setEditMode = () => {
+		setDraft([...education]);
+		setIsEditing(true);
+	};
+
+	const onSubmit = (e) => {
+		e.preventDefault();
+		setIsEditing(false);
+		setEducation([...draft]);
 	};
 
 	if (isEditing) {
 		return (
-			<div>
-				<button onClick={addEducation}>Add</button>
-				{education.map((school) => (
+			<form onSubmit={onSubmit}>
+				<h2>Education</h2>
+				<button type="button" onClick={addEducation}>
+					Add
+				</button>
+				{draft.map((school, index) => (
 					<div key={school.id}>
 						<div>----------</div>
-						<label htmlFor={`school-name-${school.id}`}>
-							School Name:{" "}
-						</label>
-						<input
-							id={`school-name-${school.id}`}
-							type="text"
-							value={school.name}
-						/>
+						<div>
+							<label htmlFor={`school-name-${school.id}`}>
+								School Name:{" "}
+							</label>
+							<input
+								id={`school-name-${school.id}`}
+								type="text"
+								onChange={(event) =>
+									onSchoolNameChange(event, index)
+								}
+								value={school.name}
+							/>
+						</div>
+						<div>
+							<label htmlFor={`school-title-${school.id}`}>
+								Diploma obtained:{" "}
+							</label>
+							<input
+								id={`school-title-${school.id}`}
+								type="text"
+								onChange={(event) =>
+									onSchoolTitleChange(event, index)
+								}
+								value={school.title}
+							/>
+						</div>
+						<div>
+							<label htmlFor={`school-date-${school.id}`}>
+								Date of obtention:{" "}
+							</label>
+							<input
+								id={`school-date-${school.id}`}
+								type="date"
+								onChange={(event) =>
+									onSchoolDateChange(event, index)
+								}
+								value={school.date}
+							/>
+						</div>
+						<div>
+							<button
+								type="button"
+								onClick={() => removeEducation(school.id)}
+							>
+								Remove
+							</button>
+						</div>
 						<div>----------</div>
 					</div>
 				))}
-			</div>
+				<div>
+					<button type="submit">Submit</button>
+				</div>
+			</form>
 		);
 	}
+
+	return (
+		<>
+			<h2>Education</h2>
+			<button type="button" onClick={setEditMode}>
+				Edit
+			</button>
+			{education.map((school) => (
+				<div key={school.id}>
+					<div>----------</div>
+					<h3>{school.name}</h3>
+					<div>
+						{school.title}
+						{school.date !== null && school.title !== ""
+							? " - "
+							: ""}
+						{school.date}
+					</div>
+					<div>----------</div>
+				</div>
+			))}
+		</>
+	);
 }
